175e6784fbd89d55aa95a1793c4a30b84f2034c6
Eden Shazar <44927208+EdenShazar@users.noreply.github.com>
2020-10-02 14:24:38 +0200

    Added useful float extension methods
    Added methods to wrap angles to a range of [-180, 180], for both degrees and radians.

---
+++ Project/Assets/Scripts/Utilities/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public static class FloatExtensionMethods
+{
+    /// <summary>
+    /// Returns the equivalent angle between -180 and 180 degrees.
+    /// </summary>
+    public static float WrapAngleDeg(this float value)
+    {
+        while (value <= -180) value += 360;
+        while (value > 180) value -= 360;
+        return value;
+    }
+
+    /// <summary>
+    /// Returns the equivalent angle between -PI and PI radians.
+    /// </summary>
+    public static float WrapAngleRad(this float value)
+    {
+        float twoPi = 2 * Mathf.PI;
+        while (value <= -Mathf.PI) value += twoPi;
+        while (value > Mathf.PI) value -= twoPi;
+        return value;
+    }
+}
