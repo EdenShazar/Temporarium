8813b28b6b03f47b42a28f3d367a07ac962fc385
syoels <syoels@gmail.com>
2020-10-03 15:52:21 +0300

    scale manager - determine characters scale (unused yet) and camera orthographic size by dragable line markers

---
+++ Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    CinemachineVirtualCamera cam;
+    CinemachineBrain brain;
+    GameObject player;
+    ScaleManager sm;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetPlayer();
+        sm = FindObjectOfType<ScaleManager>();
+        brain = FindObjectOfType<CinemachineBrain>();
+        cam = (CinemachineVirtualCamera)brain.ActiveVirtualCamera;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.m_Lens.OrthographicSize = sm.GetTargetCameraScale(player.transform.position);
+    }
+
+    public void SetPlayer() {
+        player = FindObjectOfType<PlayerController>().gameObject;
+    }
+}
