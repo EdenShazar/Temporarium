5a577303d2cf1fb404071b85a4898b47fbab4291
syoels <syoels@gmail.com>
2020-10-03 21:16:07 +0300

    added guards with radars

--- Project/Assets/Scripts/RadarController.cs
+++ Project/Assets/Scripts/RadarController.cs
@@ -4,28 +4,55 @@ using UnityEngine;
 
 public class RadarController : MonoBehaviour
 {
+    // Ellipse vars
     public float rx = 2f;
     public float ry = 1f;
     public float theta = 0f;
     Ellipse ellipse;
+    public bool isPlayerInRange = false;
+    public float angleToPlayer = 0f;
 
-    // for debugging TODO: remove
-    public bool isPlayerInRange = false; 
-    public float angleToPlayer = 0f; 
+    //scan vars
+    public float scanSpeed = 30f;
+    public float scanlineAngle = 0f;
+    public float scanWidth = 70f;
 
     // Start is called before the first frame update
     void Start()
     {
         ellipse = GetComponent<Ellipse>();
+        UpdateEllipse();
     }
 
     // Update is called once per frame
     void Update()
     {
-        angleToPlayer = (GameManager.player.transform.position - transform.position).ToVector2().GetAngleDeg();
         UpdateEllipse();
-        ellipse.Draw();
+        AdvanceScanline();
+
+        angleToPlayer = (GameManager.player.transform.position - transform.position).ToVector2().GetAngleDeg();
         isPlayerInRange = IsPointInEllipse(GameManager.player.transform.position);
+
+        DebugVizualize();
+    }
+
+    void DebugVizualize() {
+        ellipse.Draw();
+        DrawScanline(scanlineAngle - scanWidth/2f);
+        DrawScanline(scanlineAngle + scanWidth/2f);
+    }
+
+    void DrawScanline(float angle) {
+        Debug.DrawLine(transform.position,
+            new Vector3(
+            transform.position.x + rx * Mathf.Cos(angle * Mathf.PI / 180.0f),
+            transform.position.y + ry * Mathf.Sin(angle * Mathf.PI / 180.0f),
+            transform.position.z));
+    }
+
+    void AdvanceScanline() {
+        float add = (Time.deltaTime * scanSpeed);
+        scanlineAngle = ((scanlineAngle + 180f + add) % 360f) - 180f;
     }
 
     void UpdateEllipse() {
@@ -42,4 +69,21 @@ public class RadarController : MonoBehaviour
             ((Mathf.Pow(pnt.x - (ellipse.centerX + transform.position.x), 2)) / (ellipse.rX * ellipse.rX)) +
             ((Mathf.Pow(pnt.y - (ellipse.centerY + transform.position.y), 2)) / (ellipse.rY * ellipse.rY)) <= 1;
     }
+
+    public bool IsPlayerSpoetted() {
+        return IsPlayerInEllipse() && IsPlayerNearScanLine();
+    }
+
+
+    bool IsPlayerInEllipse() {
+        return IsPointInEllipse(GameManager.player.transform.position);
+    }
+
+    bool IsPlayerNearScanLine()
+    {
+        return
+            angleToPlayer >= scanWidth - scanWidth / 2 &&
+            angleToPlayer <= scanWidth + scanWidth / 2;
+    }
+
 }
