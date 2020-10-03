14b9da5de74338e6798201261af1fb3c75b52765
Eden Shazar <44927208+EdenShazar@users.noreply.github.com>
2020-10-03 13:11:04 +0200

    Added game manager and creature controller

---
+++ Project/Assets/Scripts/CreatureController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class CreatureController : MonoBehaviour
+{
+    public enum CreatureState { Alive, Egg, Extinct }
+
+    [Min(0.1f)] [SerializeField] float lifespan = 10;
+
+    CreatureState state;
+    readonly Timer timer = new Timer();
+
+    void Start()
+    {
+        SetState(CreatureState.Alive);
+    }
+
+    void Update()
+    {
+        if (state == CreatureState.Egg && Input.GetKeyDown(KeyCode.Alpha1))
+            SetState(CreatureState.Alive);
+    }
+
+    void SetState(CreatureState newState)
+    {
+        state = newState;
+
+        switch (state)
+        {
+            case CreatureState.Egg:
+                Debug.Log(gameObject.name + " died after " + System.Math.Round(timer.ElapsedTime, 2) + " seconds!");
+                
+                timer.SetPeriod(lifespan);
+                timer.Start();
+
+                timer.OnTick += EndDynasty;
+                timer.OnTick -= DieAndBirthSelf;
+                return;
+
+            case CreatureState.Alive:
+                Debug.Log(gameObject.name + " birthed itself after " + System.Math.Round(timer.ElapsedTime, 2) + " seconds!");
+
+                timer.SetPeriod(lifespan - timer.ElapsedTime);
+                timer.Start();
+
+                timer.OnTick += DieAndBirthSelf;
+                timer.OnTick -= EndDynasty;
+                return;
+
+            case CreatureState.Extinct:
+                Debug.Log(gameObject.name + " ended its dynasty " + System.Math.Round(timer.ElapsedTime, 2) + " seconds!");
+
+                timer.Stop();
+
+                timer.OnTick -= DieAndBirthSelf;
+                timer.OnTick -= EndDynasty;
+                return;
+        }
+    }
+
+    void DieAndBirthSelf()
+    {
+        SetState(CreatureState.Egg);
+    }
+
+    void EndDynasty()
+    {
+        SetState(CreatureState.Extinct);
+    }
+}
---
+++ Project/Assets/Scripts/Utilities/Timer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[Serializable]
+public class Timer
+{
+    public float Period { get; private set; } = 1f / 30f;
+    public float ElapsedTime { get; private set; }
+    
+    float startTime;
+
+
+    bool enabled = false;
+
+    public Action OnTick;
+
+    /// <param name="tickPeriod">Time interval in seconds between calls to <see cref="OnTick"/></param>
+    public void SetPeriod(float tickPeriod)
+    {
+        if (tickPeriod <= 0)
+            throw new Exception("tickInterval must be positive");
+
+        Period = tickPeriod;
+    }
+
+    public void Start()
+    {
+        if (Period == 0)
+            throw new Exception("Initialize using SetInterval(float tickInterval) before calling Start()");
+
+        if (!enabled)
+            GameManager.Instance.StartCoroutine(Count());
+        else
+            ResetTime();
+    }
+
+    public void Stop()
+    {
+        enabled = false;
+    }
+
+    IEnumerator Count()
+    {
+        enabled = true;
+        ResetTime();
+
+        while (enabled)
+        {
+            ElapsedTime = Time.time - startTime;
+            int ticks = Mathf.FloorToInt(ElapsedTime / Period);
+
+            if (ElapsedTime > Period)
+            {
+                OnTick();
+                // Reset time is not precise since it might overshoot the last tick, but it's good enough
+                ResetTime();
+            }
+
+            yield return null;
+        }
+    }
+
+    void ResetTime()
+    {
+        startTime = Time.time;
+    }
+}
